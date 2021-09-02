fb8f63760c00dc87969e07b15ab389f976112e49
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-02 00:29:24 -0400

    add search function

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -8,10 +8,106 @@
         <v-toolbar-title class="white--text"
           >{{ input.userName }}
         </v-toolbar-title>
+        <v-btn icon class="hidden-xs-only" @click="searchFlag = true">
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
       </v-app-bar>
       <v-container>
         <v-row class="mb-8 pb-14" align="end">
           <v-col>
+            <!-- search Dialog -->
+            <v-dialog v-model="searchFlag" max-width="500px">
+              <v-card>
+                <v-card-title> Search Message</v-card-title>
+                <v-col cols="12">
+                  <v-text-field
+                    v-model="searchMessage"
+                    label="Message"
+                  ></v-text-field>
+                </v-col>
+                <v-col class="d-flex justify-end">
+                  <v-btn @click="searchMessages()">search</v-btn>
+                </v-col>
+                <v-container v-if="searchList.length !== 0">
+                  <v-list>
+                    <v-list-item
+                      v-for="message of searchList"
+                      :key="message._id"
+                      :class="[
+                      'd-flex flex-row align-center my-2',
+                      message.userName == input.userName ? 'justify-end' : null,
+                    ]"
+                    >
+                      <div
+                        v-if="message.userName == input.userName"
+                        class="rounded-lg light-blue lighten-5 row-6"
+                        style="min-width: 300px; height: 150px"
+                      >
+                        <div class="col d-flex justify-start">
+                          <v-avatar :color="message.color" size="36">
+                            <span v-if="message.userName" class="white--text">
+                              {{ message.userName.charAt(0) }}
+                            </span>
+                          </v-avatar>
+                          <span class="ml-2 mt-1 d-flex"
+                            >{{ message.userName + ": " }}
+                          </span>
+                        </div>
+                        <div class="col">
+                          <span :class="['blue--text ', message.typography]"
+                            >{{ message.text }}
+                          </span>
+                        </div>
+                        <div class="col d-flex justify-end">
+                          <span>
+                            {{
+                              new Date(message.timestamp).toLocaleTimeString(
+                                "en-US"
+                              )
+                            }}
+                          </span>
+                        </div>
+                      </div>
+                      <div
+                        v-else
+                        class="rounded-lg red lighten-4 row-6"
+                        style="min-width: 300px; height: 150px"
+                      >
+                        <div class="col d-flex justify-start">
+                          <v-avatar :color="message.color" size="36">
+                            <span v-if="message.userName" class="white--text">
+                              {{ message.userName.charAt(0) }}
+                            </span>
+                          </v-avatar>
+                          <span class="ml-2 mt-1 d-flex"
+                            >{{ message.userName + ": " }}
+                          </span>
+                        </div>
+                        <div class="col">
+                          <span :class="['blue--text ', message.typography]"
+                            >{{ message.text }}
+                          </span>
+                        </div>
+                        <div class="col d-flex justify-end">
+                          <span>
+                            {{
+                              new Date(message.timestamp).toLocaleTimeString(
+                                "en-US"
+                              )
+                            }}
+                          </span>
+                        </div>
+                      </div>
+                    </v-list-item>
+                  </v-list>
+                </v-container>
+                <v-card-actions>
+                  <v-btn color="primary" text @click="searchFlag = false">
+                    Close
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
             <!-- Apollo watched Graphql query -->
             <ApolloQuery :query="require('../graphql/queries.gql')">
               <ApolloSubscribeToMore
@@ -127,12 +223,16 @@
 </template>
 
 <script>
+import SEARCHQUERY from '../graphql/search.gql';
+
 export default {
   name: 'ChatInput',
 
   data() {
     return {
+      searchFlag: false,
       newMessage: '',
+      searchMessage: '',
       input: {
         userName: '',
         timestamp: 0,
@@ -140,6 +240,7 @@ export default {
         color: '',
         text: '',
       },
+      searchList: [],
     };
   },
   created() {
@@ -174,6 +275,17 @@ export default {
           text: this.input.text,
         },
       };
+    },
+    async searchMessages() {
+      if (this.searchMessage !== '') {
+        const { data } = await this.$apollo.query({
+          query: SEARCHQUERY,
+          variables: { input: this.searchMessage },
+        });
+        this.searchList = data.searchMessage;
+        // eslint-disable-next-line no-console
+        console.log(this.searchList);
+      }
     },
   },
 };
