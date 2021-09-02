7dcccc0996615b9cbbf2229a3725be844d00512f
Michele Sanseviero <msanseviero1195@gmail.com>
2021-09-01 22:12:39 -0400

    add back bottom

--- src/components/ChatInput.vue
+++ src/components/ChatInput.vue
@@ -1,124 +1,132 @@
 <template>
-    <div id="app">
-        <v-app app>
-        <v-app-bar color="blue" app>
-            <v-toolbar-title class="white--text">{{ input.userName }} </v-toolbar-title>
-        </v-app-bar>
-        <v-container>
-            <v-row class="mb-8 pb-14" align="end">
-            <v-col>
-                <!-- Apollo watched Graphql query -->
-                <ApolloQuery :query="require('../graphql/queries.gql')">
-                <ApolloSubscribeToMore
-                    :document="require('../graphql/subscription.gql')"
-                    :updateQuery="onMessageAdded"
-                />
+  <div id="app">
+    <v-app app>
+      <v-app-bar color="blue" app>
+        <v-btn icon class="hidden-xs-only" @click="goHome()">
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+        <v-toolbar-title class="white--text"
+          >{{ input.userName }}
+        </v-toolbar-title>
+      </v-app-bar>
+      <v-container>
+        <v-row class="mb-8 pb-14" align="end">
+          <v-col>
+            <!-- Apollo watched Graphql query -->
+            <ApolloQuery :query="require('../graphql/queries.gql')">
+              <ApolloSubscribeToMore
+                :document="require('../graphql/subscription.gql')"
+                :updateQuery="onMessageAdded"
+              />
 
-                <div slot-scope="{ result: { data } }">
-                    <template v-if="data">
+              <div slot-scope="{ result: { data } }">
+                <template v-if="data">
+                  <div
+                    v-for="message of data.getMessages"
+                    :key="message._id"
+                    :class="[
+                      'd-flex flex-row align-center my-2',
+                      message.userName == input.userName ? 'justify-end' : null,
+                    ]"
+                  >
                     <div
-                        v-for="message of data.getMessages"
-                        :key="message._id"
-                        :class="[
-                        'd-flex flex-row align-center my-2',
-                        message.userName == input.userName ? 'justify-end' : null,
-                        ]"
+                      v-if="message.userName == input.userName"
+                      class="rounded-lg light-blue lighten-5 row-6"
+                      style="min-width: 300px; height: 150px"
                     >
-                        <div v-if="message.userName == input.userName"
-                        class="rounded-lg light-blue lighten-5 row-6"
-                        style="min-width: 300px; height: 150px;">
-                            <div class="col d-flex justify-start">
-                                <v-avatar
-                                :color="message.color" size="36">
-                                    <span v-if="message.userName" class="white--text">
-                                        {{ message.userName.charAt(0) }}
-                                    </span>
-                                </v-avatar>
-                                <span
-                                class="ml-2 mt-1 d-flex"
-                                >{{ message.userName + ": " }}
-                                </span>
-                            </div>
-                            <div class="col">
-                                <span
-                                :class="['blue--text ',message.typography]"
-                                >{{ message.text }}
-                                </span>
-                            </div>
-                            <div class="col d-flex justify-end">
-                                <span>
-                                {{ new Date(message.timestamp)
-                                    .toLocaleTimeString('en-US') }}
-                                </span>
-                            </div>
-
-                        </div>
-                        <div v-else
-                        class="rounded-lg red lighten-4 row-6"
-                        style="min-width: 300px; height: 150px;">
-                            <div class="col d-flex justify-start">
-                                <v-avatar :color="message.color" size="36">
-                                    <span v-if="message.userName" class="white--text">
-                                        {{ message.userName.charAt(0) }}
-                                    </span>
-                                </v-avatar>
-                                <span
-                                class="ml-2 mt-1 d-flex"
-                                >{{ message.userName + ": "}}
-                                </span>
-                            </div>
-                            <div class="col">
-                                <span
-                                :class="['blue--text ',message.typography]"
-                                >{{ message.text }}
-                                </span>
-                            </div>
-                            <div class="col d-flex justify-end">
-                                <span>
-                                {{ new Date(message.timestamp)
-                                    .toLocaleTimeString('en-US') }}
-                                </span>
-                            </div>
-                        </div>
+                      <div class="col d-flex justify-start">
+                        <v-avatar :color="message.color" size="36">
+                          <span v-if="message.userName" class="white--text">
+                            {{ message.userName.charAt(0) }}
+                          </span>
+                        </v-avatar>
+                        <span class="ml-2 mt-1 d-flex"
+                          >{{ message.userName + ": " }}
+                        </span>
+                      </div>
+                      <div class="col">
+                        <span :class="['blue--text ', message.typography]"
+                          >{{ message.text }}
+                        </span>
+                      </div>
+                      <div class="col d-flex justify-end">
+                        <span>
+                          {{
+                            new Date(message.timestamp).toLocaleTimeString(
+                              "en-US"
+                            )
+                          }}
+                        </span>
+                      </div>
                     </div>
-                    </template>
-                </div>
-                </ApolloQuery>
+                    <div
+                      v-else
+                      class="rounded-lg red lighten-4 row-6"
+                      style="min-width: 300px; height: 150px"
+                    >
+                      <div class="col d-flex justify-start">
+                        <v-avatar :color="message.color" size="36">
+                          <span v-if="message.userName" class="white--text">
+                            {{ message.userName.charAt(0) }}
+                          </span>
+                        </v-avatar>
+                        <span class="ml-2 mt-1 d-flex"
+                          >{{ message.userName + ": " }}
+                        </span>
+                      </div>
+                      <div class="col">
+                        <span :class="['blue--text ', message.typography]"
+                          >{{ message.text }}
+                        </span>
+                      </div>
+                      <div class="col d-flex justify-end">
+                        <span>
+                          {{
+                            new Date(message.timestamp).toLocaleTimeString(
+                              "en-US"
+                            )
+                          }}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </template>
+              </div>
+            </ApolloQuery>
+          </v-col>
+        </v-row>
+      </v-container>
+      <v-footer fixed>
+        <v-container class="ma-0 pa-0">
+          <v-row no-gutters>
+            <v-col>
+              <ApolloMutation
+                :mutation="require('../graphql/mutation.gql')"
+                :variables="messageInput()"
+                @done="input.text = ''"
+              >
+                <template slot-scope="{ mutate }">
+                  <div class="d-flex flex-row align-center">
+                    <v-text-field
+                      v-model="input.text"
+                      placeholder="Type a message"
+                      @keypress.enter="mutate"
+                    ></v-text-field>
+                    <v-btn icon class="ml-4" @click="mutate"
+                      ><v-icon>mdi-send</v-icon></v-btn
+                    >
+                  </div>
+                </template>
+              </ApolloMutation>
             </v-col>
-            </v-row>
+          </v-row>
         </v-container>
-        <v-footer fixed>
-            <v-container class="ma-0 pa-0">
-            <v-row no-gutters>
-                <v-col>
-                <ApolloMutation
-                    :mutation="require('../graphql/mutation.gql')"
-                    :variables="messageInput()"
-                    @done="input.text = ''"
-                >
-                    <template slot-scope="{ mutate }">
-                    <div class="d-flex flex-row align-center">
-                        <v-text-field
-                        v-model="input.text"
-                        placeholder="Type a message"
-                        @keypress.enter="mutate"
-                        ></v-text-field>
-                        <v-btn icon class="ml-4" @click="mutate"
-                        ><v-icon>mdi-send</v-icon></v-btn
-                        >
-                    </div>
-                    </template>
-                </ApolloMutation>
-                </v-col>
-            </v-row>
-            </v-container>
-        </v-footer>
-        </v-app>
-    </div>
+      </v-footer>
+    </v-app>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: 'ChatInput',
 
@@ -148,6 +156,10 @@ export default {
   },
 
   methods: {
+    goHome() {
+      sessionStorage.removeItem('user');
+      this.$router.push({ name: 'Login' });
+    },
     onMessageAdded(previousResult, { subscriptionData }) {
       previousResult.getMessages.push(subscriptionData.data.newMessage);
       return previousResult;
